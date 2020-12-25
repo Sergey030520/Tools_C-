3f23b34c5f67c26f18ab66597b633c265ffae0b6
Sergey030520 <Malipa1941@yandex.ru>
2020-12-25 19:05:31 +0300

    add tools_c++

---
+++ Tools_C++/Source.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+//call functions std::cout
+void print() {
+	//ѕотому что хотим использовать стандартную библиотеку дл€ ввыода в крнсоль и 
+	//не использовать не какие другие
+	std::cout << "Hello Skillbox!" << std::endl;
+}
+
+/*
+ѕрисваиваем занчение переменной и выводим hello world
+*/
+
+int main() {
+	int x = 100;
+	3 + 7;
+	int y = x + 100;
+	int b = 0;
+	b = b + 2;
+	
+	int test;
+	test = 100500;
+
+	int test2 = 1005001;
+
+	int mult = x * y;
+	int random = 1002;
+	std::cout << random << std::endl;
+}
